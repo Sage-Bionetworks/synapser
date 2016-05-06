d46474aa01feed76d7be891157227b2a54fa3989
bhoff <bruce.hoff@sagebase.org>
2016-05-05 20:11:52 -0700

    white listed two methods, for demo purposes

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,7 +7,20 @@ library(testthat)
 library("synapse")
 
 test_check("synapse", filter="unit")
-if (FALSE) { # if credentials are available
+
+canExecute<-TRUE
+
+username<-Sys.getenv("SYNAPSE_USERNAME")
+if (nchar(username)==0) {
+	message("WARNING: Cannot run integration test.  Environment variable SYNAPSE_USERNAME is missing.")
+	canExecute<-FALSE
+}
+apiKey<-Sys.getenv("SYNAPSE_APIKEY")
+if (nchar(apiKey)==0) {
+	message("WARNING: Cannot run integration test.  Environment variable SYNAPSE_APIKEY is missing.")
+	canExecute<-FALSE
+}
+
+if (canExecute) {
 	test_check("synapse", filter="integration")
-	
 }
