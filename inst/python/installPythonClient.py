ae451797f562dae970926590757c2da50a16c6eb
bhoff <bruce.hoff@sagebase.org>
2017-06-01 16:56:21 -0700

    installPythonClient.py

--- inst/python/installPythonClient.py
+++ inst/python/installPythonClient.py
@@ -103,7 +103,7 @@ def installPackage(packageName, linkPrefix, path):
         # TODO this is a hacky solution. distutils.core.run_setup is supposed to be the one modifying sys.argv
         # it is able to do so on my local python but not on this compiled python
         # TODO how do we get 'setup.py' to install into inst/lib?
-        distutils.core.run_setup(script_name='setup.py', script_args=['install', '--user', 'quiet', '2'])
+        distutils.core.run_setup(script_name='setup.py', script_args=['install', '--user'])
         # step back one level before remove the directory
         os.chdir(path)
         shutil.rmtree(packageDir)
