d88c159285daaa010774832f66cfc13494ab5ed7
bhoff <bruce.hoff@sagebase.org>
2017-06-01 17:14:56 -0700

    installPythonClient.py

--- inst/python/installPythonClient.py
+++ inst/python/installPythonClient.py
@@ -28,57 +28,26 @@ def main(path):
     sys.path.insert(0, moduleInstallationFolder)
     # The preferred approach is to use pip...
     
-    origStdout=sys.stdout
-    origStderr=sys.stderr
+    call_pip(['install', 'pip',  '--upgrade'])
+    call_pip(['install', '--user', 'urllib3',  '--upgrade'])
+    call_pip(['install', '--user', 'requests',  '--upgrade'])
+    call_pip(['install', '--user', 'six',  '--upgrade'])
+    call_pip(['install', '--user', 'backports.csv',  '--upgrade'])
     
-    outfile=tempfile.mkstemp()
-    outfilehandle=outfile[0]
-    outfilepath=outfile[1]
-    outfilehandle = open(outfilepath, 'w')
-    sys.stdout = outfilehandle
-    sys.stderr = outfilehandle
-    
-    print(sys.stdout.errors)
-    print(sys.stderr.errors)
-    print(sys.stdout.isatty())
-    print(sys.stderr.isatty())
-    
-    try:
-        call_pip(['install', 'pip',  '--upgrade'])
-        
-        print("After install - pip - --upgrade")
-        
-        
-        call_pip(['install', '--user', 'urllib3',  '--upgrade'])
-        call_pip(['install', '--user', 'requests',  '--upgrade'])
-        call_pip(['install', '--user', 'six',  '--upgrade'])
-        call_pip(['install', '--user', 'backports.csv',  '--upgrade'])
-        
-        # ...but - for some reason - pip breaks when we install future and the python synapse client
-        # my guess is that pip 'shells out' to call setup.py and hops to another version of
-        # python on the machine
+    # ...but - for some reason - pip breaks when we install future and the python synapse client
+    # my guess is that pip 'shells out' to call setup.py and hops to another version of
+    # python on the machine
 
-        
-        #pip.main(['install', '--user', 'future',  '--upgrade'])
-        packageName = "future-0.15.2"
-        linkPrefix = "https://pypi.python.org/packages/5a/f4/99abde815842bc6e97d5a7806ad51236630da14ca2f3b1fce94c0bb94d3d/"
-        installPackage(packageName, linkPrefix, path)
     
-        #pip.main(['install', '--user', 'synapseclient',  '--upgrade'])
-        packageName = "synapseclient-1.6.1"
-        linkPrefix = "https://pypi.python.org/packages/37/fd/5672e85abc68f4323e19e470cb7eeb0f8dc610566f124c930c3026404fb9/"
-        installPackage(packageName, linkPrefix, path)
-    finally:
-        sys.stdout=origStdout
-        sys.stderr=origStderr
-        # Next line has error: ValueError: underlying buffer has been detached
-        outfilehandle.flush()
-        outfilehandle.close()
-        with open(outfilepath, 'r') as f:
-            print(f.read())
-        # The following causes an error: The process cannot access the file because it is being used by another process:
-        # os we'll let the system remove the temp file
-        # os.remove(outfilepath)
+    #pip.main(['install', '--user', 'future',  '--upgrade'])
+    packageName = "future-0.15.2"
+    linkPrefix = "https://pypi.python.org/packages/5a/f4/99abde815842bc6e97d5a7806ad51236630da14ca2f3b1fce94c0bb94d3d/"
+    installPackage(packageName, linkPrefix, path)
+
+    #pip.main(['install', '--user', 'synapseclient',  '--upgrade'])
+    packageName = "synapseclient-1.6.1"
+    linkPrefix = "https://pypi.python.org/packages/37/fd/5672e85abc68f4323e19e470cb7eeb0f8dc610566f124c930c3026404fb9/"
+    installPackage(packageName, linkPrefix, path)
             
 def installPackage(packageName, linkPrefix, path):
     # download 
@@ -113,7 +82,27 @@ def installPackage(packageName, linkPrefix, path):
         os.remove(localZipFile)
     
 def call_pip(args):
+    origStdout=sys.stdout
+    origStderr=sys.stderr 
+    outfile=tempfile.mkstemp()
+    outfilehandle=outfile[0]
+    outfilepath=outfile[1]
+    outfilehandle = open(outfilepath, 'w')
+    sys.stdout = outfilehandle
+    sys.stderr = outfilehandle
+    
+    try:
         rc = pip.main(args)
         if rc!=0:
             raise('pip.main returned '+str(rc))
-
+    finally:
+        sys.stdout=origStdout
+        sys.stderr=origStderr
+        # Next line has error: ValueError: underlying buffer has been detached
+        outfilehandle.flush()
+        outfilehandle.close()
+        with open(outfilepath, 'r') as f:
+            print(f.read())
+        # The following causes an error: The process cannot access the file because it is being used by another process:
+        # os we'll let the system remove the temp file
+        # os.remove(outfilepath)
