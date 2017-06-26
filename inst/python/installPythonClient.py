3a832acf6e056bfb178ac97f663e37968e90958f
bhoff <bruce.hoff@sagebase.org>
2017-06-26 13:43:32 -0700

    restored separate installation of dependencies

--- inst/python/installPythonClient.py
+++ inst/python/installPythonClient.py
@@ -34,28 +34,25 @@ def main(path):
 #     sys.path.insert(0, moduleInstallationFolder)
 #     os.environ['PYTHONPATH'] = moduleInstallationFolder
     
-#    try:
-        # The preferred approach is to use pip...
-#         call_pip('pip', moduleInstallationFolder)
-#         call_pip('urllib3', moduleInstallationFolder)
-#         call_pip('requests', moduleInstallationFolder)
-#         call_pip('six', moduleInstallationFolder)
-#         call_pip('backports.csv', moduleInstallationFolder)
-        
-        # ...but - for some reason - pip breaks when we install future and the python synapse client
-        # my guess is that pip 'shells out' to call setup.py and hops to another version of
-        # python on the machine
+
+    # The preferred approach is to use pip...
+    call_pip('pip')
+    call_pip('urllib3')
+    call_pip('requests')
+    call_pip('six')
+    call_pip('backports.csv')
     
-#         packageName = "future-0.15.2"
-#         linkPrefix = "https://pypi.python.org/packages/5a/f4/99abde815842bc6e97d5a7806ad51236630da14ca2f3b1fce94c0bb94d3d/"
-#         installPackage(packageName, linkPrefix, path, moduleInstallationFolder)
+    # ...but - for some reason - pip breaks when we install future and the python synapse client
+    # my guess is that pip 'shells out' to call setup.py and hops to another version of
+    # python on the machine
+
+    packageName = "future-0.15.2"
+    linkPrefix = "https://pypi.python.org/packages/5a/f4/99abde815842bc6e97d5a7806ad51236630da14ca2f3b1fce94c0bb94d3d/"
+    installPackage(packageName, linkPrefix, path)
     
     packageName = "synapseclient-1.7.1"
     linkPrefix = "https://pypi.python.org/packages/56/da/e489aad73886e6572737ccfe679b3a2bc9e68b05636d4ac30302d0dcf261/"
     installPackage(packageName, linkPrefix, path)
-    
-#    finally:
-#        os.environ['PYTHONPATH'] = orig_python_path
         
             
 def installPackage(packageName, linkPrefix, path):
@@ -66,12 +63,16 @@ def installPackage(packageName, linkPrefix, path):
     saveFile = open(localZipFile,'wb')
     saveFile.write(x.read())
     saveFile.close()
+    
+    print("Downloaded "+zipFileName)
 
     tar = tarfile.open(localZipFile)
     moduleInstallationFolder=path+os.sep+"inst"
     tar.extractall(path=moduleInstallationFolder)
     tar.close()
     os.remove(localZipFile)
+    
+    print("Un-tarred "+localZipFile)
     
     packageDir = moduleInstallationFolder+os.sep+packageName
     os.chdir(packageDir)
@@ -112,37 +113,38 @@ def installPackage(packageName, linkPrefix, path):
         # os we'll let the system remove the temp file
         # os.remove(outfilepath)
 
+        print("Removing "+packageDir)
         # step back one level before removing the directory
         os.chdir(path)
         shutil.rmtree(packageDir)
 
 
-# def call_pip(packageName, moduleInstallationFolder):
-#     origStdout=sys.stdout
-#     origStderr=sys.stderr 
-#     outfile=tempfile.mkstemp()
-#     outfilehandle=outfile[0]
-#     outfilepath=outfile[1]
-#     outfilehandle = open(outfilepath, 'w')
-#     sys.stdout = outfilehandle
-#     sys.stderr = outfilehandle
-#     
-#     try:
-#         rc = pip.main(['install', packageName,  '--upgrade', '--quiet', '--target', moduleInstallationFolder])
-#         if rc!=0:
-#             raise Exception('pip.main returned '+str(rc))
-#     finally:
-#         sys.stdout=origStdout
-#         sys.stderr=origStderr
-#         try:
-#             outfilehandle.flush()
-#             outfilehandle.close()
-#         except:
-#             pass # nothing to do
-#         print("-------------This is the accumulated output of 'pip.main': -------------")
-#         with open(outfilepath, 'r') as f:
-#             print(f.read())
-#         print("------------- DONE -------------")
+def call_pip(packageName):
+    origStdout=sys.stdout
+    origStderr=sys.stderr 
+    outfile=tempfile.mkstemp()
+    outfilehandle=outfile[0]
+    outfilepath=outfile[1]
+    outfilehandle = open(outfilepath, 'w')
+    sys.stdout = outfilehandle
+    sys.stderr = outfilehandle
+     
+    try:
+        rc = pip.main(['install', packageName,  '--upgrade', '--quiet'])
+        if rc!=0:
+            raise Exception('pip.main returned '+str(rc))
+    finally:
+        sys.stdout=origStdout
+        sys.stderr=origStderr
+        try:
+            outfilehandle.flush()
+            outfilehandle.close()
+        except:
+            pass # nothing to do
+        print("-------------This is the accumulated output of 'pip.main': -------------")
+        with open(outfilepath, 'r') as f:
+            print(f.read())
+        print("------------- DONE -------------")
 
 
 
