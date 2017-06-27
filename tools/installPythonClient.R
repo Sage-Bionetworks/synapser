0ed9070e0b4bb37899c46cab5a372303db9fc6e4
bhoff <bruce.hoff@sagebase.org>
2017-06-27 12:30:33 -0700

    debugging failed windows build

--- tools/installPythonClient.R
+++ tools/installPythonClient.R
@@ -17,7 +17,7 @@ pyImport("sys")
 pyExec(sprintf("sys.path.append(\"%s\")", file.path(baseDir, "inst", "python")))
 
 pyImport("installPythonClient")
-command<-sprintf("installPythonClient.main(\"%s\")", baseDir)
+command<-sprintf("installPythonClient.main('%s')", baseDir)
 pyExec("print(dir(installPythonClient))")
 message("installPythonClient.R:  imported installPythonClient, next will call ", command)
 pyExec(command)
