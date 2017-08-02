8ea0abaaa6c443f33411142c01bd6c5cf14b6822
bhoff <bruce.hoff@sagebase.org>
2017-08-01 21:23:51 -0700

    SYNR-1105

--- inst/python/gateway.py
+++ inst/python/gateway.py
@@ -2,6 +2,7 @@ import sys
 import os
 import tempfile
 import synapseclient
+import types
 
 
 def stdouterrCapture(function, abbreviateStackTrace=True):
@@ -43,10 +44,31 @@ def stdouterrCapture(function, abbreviateStackTrace=True):
             raise Exception(str(exceptionToRaise))
         else:
             raise exceptionToRaise
+        
+class GeneratorWrapper():
+    def __init__(self, wrapped):
+       object.__setattr__(self, 'inner', wrapped)
+
+    def __getattr__(self, attr):
+        return getattr(self.inner, attr)
+
+    def __setattr__(self, attr, value):
+        setattr(self.inner, attr, value)
+    
+    def nextElem(self):
+        return self.__next__()
+
+# from https://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance#2982
+def generatorModifier(g):
+    # add a public method to get the next value from a generator or iterator
+    if isinstance(g, types.GeneratorType):
+        return GeneratorWrapper(g)
+    else:
+        return g
 
 # args[0] is an object and args[1] is a method name.  args[2:] and kwargs are the method's arguments
 def invoke(*args, **kwargs):
     method_to_call = getattr(args[0], args[1])
-    return stdouterrCapture(lambda: method_to_call(*args[2:], **kwargs))
+    return generatorModifier(stdouterrCapture(lambda: method_to_call(*args[2:], **kwargs)))
 
                     
