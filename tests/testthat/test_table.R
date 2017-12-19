86285d85c4b2e26c500964090cc1593a9a477550
Kimyen <kimyen0816@gmail.com>
2017-12-19 14:00:32 -0800

    typo

--- tests/testthat/test_table.R
+++ tests/testthat/test_table.R
@@ -1,6 +1,6 @@
 context("test table utilities")
 
-test_that("data.frame can be write to and read from csv consistently", {
+test_that("data.frame can be written to and read from csv consistently", {
   a = c(3.5, NaN, Inf, -Inf, NA)
   b = c("Hello", "World", NA, "!", NA)
   expect_equal("numeric", class(a))
@@ -15,7 +15,7 @@ test_that("data.frame can be write to and read from csv consistently", {
   expect_equal(b, df2$b)
 })
 
-test_that("empty data.frame can be write to and read from csv consistently", {
+test_that("empty data.frame can be written to and read from csv consistently", {
   df <- data.frame()
   expect_equal("data.frame", class(df))
 
