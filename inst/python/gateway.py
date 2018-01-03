421a43c0d2c107df1662535e6af5c0c3080941bb
Kimyen <kimyen0816@gmail.com>
2018-01-03 13:14:38 -0800

    allow user to use either nextElem() or as.list()

--- inst/python/gateway.py
+++ inst/python/gateway.py
@@ -7,10 +7,22 @@ from synapseclient.annotations import  Annotations
 class GeneratorWrapper():
     def __init__(self, wrapped):
        self._inner = wrapped
-       self._list = list(self._inner)
+       self._use_list = False
+       self._use_iter = False
+
+    def nextElem(self):
+      if self._use_list:
+        raise "asList() has been used."
+      self._use_iter = True
+      return self._inner.__next__()
 
     def asList(self):
-        return self._list
+      if self._use_iter:
+        raise "nextElem() has been used."
+      if self._use_list:
+        raise "asList() can be used only once."
+      self._use_list = True
+      return list(self._inner)
 
 # from https://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance#2982
 def generatorModifier(g):
@@ -19,7 +31,7 @@ def generatorModifier(g):
         return GeneratorWrapper(g)
     else:
         return g
-    
+
 def annotationsModifier(a):
     # convert to a simple dictionary
     if isinstance(a, Annotations):
@@ -27,9 +39,7 @@ def annotationsModifier(a):
     else:
         return a
 
-
 # args[0] is an object and args[1] is a method name.  args[2:] and kwargs are the method's arguments
 def invoke(*args, **kwargs):
     method_to_call = getattr(args[0], args[1])
     return annotationsModifier(generatorModifier(stdouterrCapture(lambda: method_to_call(*args[2:], **kwargs), abbreviateStackTrace=True)))
-                
