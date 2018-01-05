ae46a4d99b98ca172442c27b0371dc8a0752a0dd
bhoff <bruce.hoff@sagebase.org>
2018-01-04 16:47:03 -0800

    SYNR-1125

---
+++ inst/python/interruptCheck.py
@@ -0,0 +1,16 @@
+import ctypes
+from synapseclient import dozer
+
+class InterruptChecker:
+    def __init__(self, sharedLibrary):
+        peirModule=ctypes.CDLL(sharedLibrary)
+        self.peirModule=peirModule
+        
+    def __call__(self):
+        result=self.peirModule.checkInterrupt()
+        if result!=0:
+            raise ValueError("keyboard interrupt received")
+
+def registerInterruptChecker(sharedLibrary):
+    interruptChecker = InterruptChecker(sharedLibrary)
+    dozer.add_listener(interruptChecker)
