8034a19801c266d25f7f9e78ebb9ac09e3bee560
Kimyen <kimyen0816@gmail.com>
2018-02-16 12:32:22 -0800

    style

--- R/table.R
+++ R/table.R
@@ -11,21 +11,22 @@
   if (nrow(dataFrame) != 0) {
     for (i in 1:dim(dataFrame)[2]) {
       if (is.numeric(dataFrame[[i]])) {
-        dataFrame[[i]][is.nan(dataFrame[[i]])]<-"NaN"
+        dataFrame[[i]][is.nan(dataFrame[[i]])] <- "NaN"
       } else if (is(dataFrame[[i]], "POSIXct")) {
         # convert POSIXct before uploading to Synapse
-        dataFrame[[i]]<-format(as.POSIXlt(dataFrame[[i]], 'UTC', usetz=TRUE), "%Y-%m-%d %H:%M:%S.000")
+        dataFrame[[i]] <- format(as.POSIXlt(dataFrame[[i]], "UTC", usetz = TRUE), "%Y-%m-%d %H:%M:%S.000")
       }
     }
   }
-  write.csv(x=dataFrame, file=filePath, row.names=FALSE, na="")
+  write.csv(x = dataFrame, file = filePath, row.names = FALSE, na = "")
 }
 
 # reading a csv file and returning a data.frame
 .readCsv <- function(filePath) {
   tryCatch({
-    read.csv(filePath, encoding="UTF-8", stringsAsFactors=FALSE, check.names=FALSE, na.strings=c(""))
-  }, error = function(e) {
+    read.csv(filePath, encoding = "UTF-8", stringsAsFactors = FALSE, check.names = FALSE, na.strings = c(""))
+  },
+  error = function(e) {
     stopifnot(e$message == "first five rows are empty: giving up")
     data.frame()
   })
