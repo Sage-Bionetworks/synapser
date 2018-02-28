dc39fe56a1e474e05eae9e7ed6d2d2ca549228a3
bhoff <bruce.hoff@sagebase.org>
2018-02-28 11:48:25 -0800

    SYNR-1240

--- R/zzz.R
+++ R/zzz.R
@@ -3,138 +3,138 @@
 # Author: bhoff
 ###############################################################################
 
-.onLoad <- function(libname, pkgname) { 
-	.addPythonAndFoldersToSysPath(system.file(package="synapser"))
-	
-	.defineRPackageFunctions()
-	# .defineOverloadFunctions() must come AFTER .defineRPackageFunctions() 
-	# because it redefines selected generic functions.
-	.defineOverloadFunctions()
-	
-	pyImport("synapseclient")
-	pySet("synapserVersion", sprintf("synapser/%s ", packageVersion("synapser")))
-	pyExec("synapseclient.USER_AGENT['User-Agent'] = synapserVersion + synapseclient.USER_AGENT['User-Agent']")
-	pyExec("syn=synapseclient.Synapse()")
-	
-	# register interrupt check
-	libraryName<-sprintf("PythonEmbedInR%s", .Platform$dynlib.ext)
-	if(.Platform$OS.type == "windows") {
-		sharedLibrary<-libraryName
-	} else {
-		sharedLibraryLocation<-system.file("libs", package="PythonEmbedInR")
-		sharedLibrary<-file.path(sharedLibraryLocation, libraryName)
-	}
-	pyImport("interruptCheck")
-	pyExec(sprintf("interruptCheck.registerInterruptChecker('%s')", sharedLibrary))	
+.onLoad <- function(libname, pkgname) {
+  .addPythonAndFoldersToSysPath(system.file(package = "synapser"))
+
+  .defineRPackageFunctions()
+  # .defineOverloadFunctions() must come AFTER .defineRPackageFunctions()
+  # because it redefines selected generic functions
+  .defineOverloadFunctions()
+
+  pyImport("synapseclient")
+  pySet("synapserVersion", sprintf("synapser/%s ", packageVersion("synapser")))
+  pyExec("synapseclient.USER_AGENT['User-Agent'] = synapserVersion + synapseclient.USER_AGENT['User-Agent']")
+  pyExec("syn=synapseclient.Synapse()")
+
+  # register interrupt check
+  libraryName <- sprintf("PythonEmbedInR%s", .Platform$dynlib.ext)
+  if (.Platform$OS.type == "windows") {
+    sharedLibrary <- libraryName
+  } else {
+    sharedLibraryLocation <- system.file("libs", package = "PythonEmbedInR")
+    sharedLibrary <- file.path(sharedLibraryLocation, libraryName)
+  }
+  pyImport("interruptCheck")
+  pyExec(sprintf("interruptCheck.registerInterruptChecker('%s')", sharedLibrary))
 }
 
-.determineArgsAndKwArgs<-function(...) {
-	values<-list(...)
-	valuenames<-names(values)
-	n<-length(values)
-	args<-list()
-	kwargs<-list()
-	if (n>0) {
-		positionalArgument<-TRUE
-		for (i in 1:n) {
-			if (is.null(valuenames) || length(valuenames[[i]])==0 || nchar(valuenames[[i]])==0) {
-				# it's a positional argument
-				if (!positionalArgument) {
-					stop("positional argument follows keyword argument")
-				}
-				if (is.null(values[[i]])) {
-					# inserting a value into a list at best is a no-op, at worst removes an existing value
-					# to get the desired insertion we must wrap it in a list
-					args[length(args)+1]<-list(NULL)
-				} else {
-					args[[length(args)+1]]<-values[[i]]
-				}
-			} else {
-				# It's a keyword argument.  All subsequent arguments must also be keyword arg's
-				positionalArgument<-FALSE
-				# a repeated value will overwite an earlier one
-				if (is.null(values[[i]])) {
-					# inserting a value into a list at best is a no-op, at worst removes an existing value
-					# to get the desired insertion we must wrap it in a list
-					kwargs[valuenames[[i]]]<-list(NULL)
-				} else {
-					kwargs[[valuenames[[i]]]]<-values[[i]]
-				}
-			}
-		}
-	}
-	list(args=args, kwargs=kwargs)
+.determineArgsAndKwArgs <- function(...) {
+  values <- list(...)
+  valuenames <- names(values)
+  n <- length(values)
+  args <- list()
+  kwargs <- list()
+  if (n > 0) {
+    positionalArgument <- TRUE
+    for (i in 1:n) {
+      if (is.null(valuenames) || length(valuenames[[i]]) == 0 || nchar(valuenames[[i]]) == 0) {
+        # it's a positional argument
+        if (!positionalArgument) {
+          stop("positional argument follows keyword argument")
+        }
+        if (is.null(values[[i]])) {
+          # inserting a value into a list at best is a no-op, at worst removes an existing value
+          # to get the desired insertion we must wrap it in a list
+          args[length(args) + 1] <- list(NULL)
+        } else {
+          args[[length(args) + 1]] <- values[[i]]
+        }
+      } else {
+        # It's a keyword argument.  All subsequent arguments must also be keyword arg's
+        positionalArgument <- FALSE
+        # a repeated value will overwite an earlier one
+        if (is.null(values[[i]])) {
+          # inserting a value into a list at best is a no-op, at worst removes an existing value
+          # to get the desired insertion we must wrap it in a list
+          kwargs[valuenames[[i]]] <- list(NULL)
+        } else {
+          kwargs[[valuenames[[i]]]] <- values[[i]]
+        }
+      }
+    }
+  }
+  list(args = args, kwargs = kwargs)
 }
 
-.cleanUpStackTrace<-function(callable, args) {
-	conn<-textConnection("outputCapture", open="w", local=TRUE)
-	sink(conn)
-	tryCatch({
-			result<-do.call(callable, args)
-			sink()
-			close(conn)
-			cat(outputCapture, "\n")
-			result
-		}, 
-		error = function(e) {
-			sink()
-			close(conn)
-			errorToReport<-paste(c(outputCapture, e$message), collapse="\n")
-			if (!getOption("verbose")) {
-				# extract the error message
-				splitArray<-strsplit(errorToReport, "exception-message-boundary", fixed=TRUE)[[1]]
-				if (length(splitArray)>=2) errorToReport<-splitArray[2]
-			}
-			stop(errorToReport)
-		}
-	)
+.cleanUpStackTrace <- function(callable, args) {
+  conn <- textConnection("outputCapture", open = "w", local=TRUE)
+  sink(conn)
+  tryCatch({
+    result <- do.call(callable, args)
+    sink()
+    close(conn)
+    cat(paste(outputCapture, collapse = ""))
+    result
+  },
+  error = function(e) {
+    sink()
+    close(conn)
+    errorToReport <- paste(c(outputCapture, e$message), collapse = "\n")
+    if (!getOption("verbose")) {
+      # extract the error message
+      splitArray <- strsplit(errorToReport, "exception-message-boundary", fixed = TRUE)[[1]]
+      if (length(splitArray) >= 2) errorToReport <- splitArray[2]
+    }
+    stop(errorToReport)
+  }
+  )
 }
 
-.defineFunction<-function(synName, pyName, functionContainerName) {
-	force(synName)
-	force(pyName)
-	force(functionContainerName)
-	assign(sprintf(".%s", synName), function(...) {
-				functionContainer<-pyGet(functionContainerName, simplify=FALSE)
-				argsAndKwArgs<-.determineArgsAndKwArgs(...)
-				functionAndArgs<-append(list(functionContainer, pyName), argsAndKwArgs$args)
-				returnedObject <- .cleanUpStackTrace(pyCall, list("gateway.invoke", args=functionAndArgs, kwargs=argsAndKwArgs$kwargs, simplify=F))
-				.objectDefinitionHelper(returnedObject)
-			})
-	setGeneric(
-			name=synName,
-			def = function(...) {
-				do.call(sprintf(".%s", synName), args=list(...))
-			}
-	)
+.defineFunction <- function(synName, pyName, functionContainerName) {
+  force(synName)
+  force(pyName)
+  force(functionContainerName)
+  assign(sprintf(".%s", synName), function(...) {
+    functionContainer <- pyGet(functionContainerName, simplify = FALSE)
+    argsAndKwArgs <- .determineArgsAndKwArgs(...)
+    functionAndArgs <- append(list(functionContainer, pyName), argsAndKwArgs$args)
+    returnedObject <- .cleanUpStackTrace(pyCall, list("gateway.invoke", args = functionAndArgs, kwargs = argsAndKwArgs$kwargs, simplify = F))
+    .objectDefinitionHelper(returnedObject)
+  })
+  setGeneric(
+    name = synName,
+    def = function(...) {
+      do.call(sprintf(".%s", synName), args = list(...))
+    }
+  )
 }
 
-.defineConstructor<-function(synName, pyName) {
-	force(synName)
-	force(pyName)
-	assign(sprintf(".%s", synName), function(...) {
-				synapseClientModule<-pyGet("synapseclient")
-				argsAndKwArgs<-.determineArgsAndKwArgs(...)
-				functionAndArgs<-append(list(synapseClientModule, pyName), argsAndKwArgs$args)
-				.cleanUpStackTrace(pyCall, list("gateway.invoke", args=functionAndArgs, kwargs=argsAndKwArgs$kwargs, simplify=F))
-			})
-	setGeneric(
-			name=synName,
-			def = function(...) {
-				do.call(sprintf(".%s", synName), args=list(...))
-			}
-	)
+.defineConstructor <- function(synName, pyName) {
+  force(synName)
+  force(pyName)
+  assign(sprintf(".%s", synName), function(...) {
+    synapseClientModule <- pyGet("synapseclient")
+    argsAndKwArgs <- .determineArgsAndKwArgs(...)
+    functionAndArgs <- append(list(synapseClientModule, pyName), argsAndKwArgs$args)
+    .cleanUpStackTrace(pyCall, list("gateway.invoke", args = functionAndArgs, kwargs = argsAndKwArgs$kwargs, simplify = F))
+  })
+  setGeneric(
+    name = synName,
+    def = function(...) {
+      do.call(sprintf(".%s", synName), args = list(...))
+    }
+  )
 }
 
-.defineRPackageFunctions<-function() {
-	functionInfo<-.getSynapseFunctionInfo(system.file(package="synapser"))
-	for (f in functionInfo) {
-		.defineFunction(f$synName, f$name, f$functionContainerName)
-	}
-	classInfo<-.getSynapseClassInfo(system.file(package="synapser"))
-	for (c in classInfo) {
-		.defineConstructor(c$name, c$name)
-	}
+.defineRPackageFunctions <- function() {
+  functionInfo <- .getSynapseFunctionInfo(system.file(package = "synapser"))
+  for (f in functionInfo) {
+    .defineFunction(f$synName, f$name, f$functionContainerName)
+  }
+  classInfo <- .getSynapseClassInfo(system.file(package = "synapser"))
+  for (c in classInfo) {
+    .defineConstructor(c$name, c$name)
+  }
 }
 
 .objectDefinitionHelper <- function(object) {
@@ -153,28 +153,28 @@
 }
 
 .onAttach <- function(libname, pkgname) {
-	tou <- "\nTERMS OF USE NOTICE:
-	When using Synapse, remember that the terms and conditions of use require that you:
-	1) Attribute data contributors when discussing these data or results from these data.
-	2) Not discriminate, identify, or recontact individuals or groups represented by the data.
-	3) Use and contribute only data de-identified to HIPAA standards.
-	4) Redistribute data only under these same terms of use.\n"
-	
-	packageStartupMessage(tou)
+  tou <- "\nTERMS OF USE NOTICE:
+  When using Synapse, remember that the terms and conditions of use require that you:
+  1) Attribute data contributors when discussing these data or results from these data.
+  2) Not discriminate, identify, or recontact individuals or groups represented by the data.
+  3) Use and contribute only data de-identified to HIPAA standards.
+  4) Redistribute data only under these same terms of use.\n"
+
+  packageStartupMessage(tou)
 }
 
-.defineOverloadFunctions<-function() {
+.defineOverloadFunctions <- function() {
   assign(".Table", function(...) {
-    synapseClientModule<-pyGet("synapseclient")
-    argsAndKwArgs<-.determineArgsAndKwArgs(...)
-    functionAndArgs<-append(list(synapseClientModule, "Table"), argsAndKwArgs$args)
-    returnedObject <- .cleanUpStackTrace(pyCall, list("gateway.invoke", args=functionAndArgs, kwargs=argsAndKwArgs$kwargs, simplify=F))
+    synapseClientModule <- pyGet("synapseclient")
+    argsAndKwArgs <- .determineArgsAndKwArgs(...)
+    functionAndArgs <- append(list(synapseClientModule, "Table"), argsAndKwArgs$args)
+    returnedObject <- .cleanUpStackTrace(pyCall, list("gateway.invoke", args = functionAndArgs, kwargs = argsAndKwArgs$kwargs, simplify = F))
     .objectDefinitionHelper(returnedObject)
   })
   setGeneric(
-    name="Table",
+    name = "Table",
     def = function(schema, values, ...) {
-      do.call(".Table", args=list(schema, values, ...))
+      do.call(".Table", args = list(schema, values, ...))
     }
   )
   setMethod(
@@ -203,12 +203,12 @@
       x$asList()
     })
 
-	setGeneric(
-	  name="nextElem",
-	  def = function(x) {
-	    standardGeneric("nextElem")
-	  }
-	)
+  setGeneric(
+    name = "nextElem",
+    def = function(x) {
+      standardGeneric("nextElem")
+    }
+  )
 
   setMethod(
     f = "nextElem",
@@ -217,4 +217,3 @@
       x$nextElem()
     })
 }
-
