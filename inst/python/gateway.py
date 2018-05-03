3f6e03af61bdc5e3b9b1d9e588b25cc4e36107bf
Kimyen <kimyen0816@gmail.com>
2018-05-03 11:27:42 -0700

    gateway.py is override the PythonEmbedInR version
    so it must have all content in the PythonEmbedInR version

--- inst/python/gateway.py
+++ inst/python/gateway.py
@@ -1,8 +1,35 @@
-
 import synapseclient
 import types
 from stdouterrCapture import stdouterrCapture
 from synapseclient.annotations import  Annotations
+
+class GeneratorWrapper():
+    def __init__(self, wrapped):
+       self._inner = wrapped
+       self._use_list = False
+       self._use_iter = False
+
+    def nextElem(self):
+      if self._use_list:
+        raise Exception("Have already enumerated all elements.")
+      self._use_iter = True
+      return self._inner.__next__()
+
+    def asList(self):
+      if self._use_iter:
+        raise Exception("Can't generate a list once enumeration has begun.")
+      if self._use_list:
+        raise Exception("Have already enumerated all elements.")
+      self._use_list = True
+      return list(self._inner)
+
+# from https://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance#2982
+def generatorModifier(g):
+    # add a public method to get the next value from a generator or iterator
+    if isinstance(g, types.GeneratorType):
+        return GeneratorWrapper(g)
+    else:
+        return g
 
 def annotationsModifier(a):
     # convert to a simple dictionary
@@ -14,4 +41,4 @@ def annotationsModifier(a):
 # args[0] is an object and args[1] is a method name.  args[2:] and kwargs are the method's arguments
 def invoke(*args, **kwargs):
     method_to_call = getattr(args[0], args[1])
-    return annotationsModifier(stdouterrCapture(lambda: method_to_call(*args[2:], **kwargs), abbreviateStackTrace=True))
+    return annotationsModifier(generatorModifier(stdouterrCapture(lambda: method_to_call(*args[2:], **kwargs), abbreviateStackTrace=True)))
