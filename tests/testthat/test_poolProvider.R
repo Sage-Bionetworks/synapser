f929aca763a08adec07246b93fa25f2b1e951bfa
Kimyen <kimyen0816@gmail.com>
2018-08-15 11:59:35 -0700

    debug

--- tests/testthat/test_poolProvider.R
+++ tests/testthat/test_poolProvider.R
@@ -5,8 +5,9 @@ test_that("synapser uses Synapse Python Client pool provider for single thread",
   callback <- function(name, def) {
     setGeneric(name, def)
   }
-
+  PythonEmbedInR::pyImport("sys")
   PythonEmbedInR::pyExec(sprintf("sys.path.insert(0, '.')"))
+  PythonEmbedInR::pyExecp("sys.path")
   PythonEmbedInR::pyImport("testPoolProvider")
   PythonEmbedInR::generateRWrappers(pyPkg = "testPoolProvider",
                                     container = "testPoolProvider",
