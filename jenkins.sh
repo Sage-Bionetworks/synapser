b24830a246353d15f8f65f0218ffc8bea42a0e05
KimYen Ladia <kimyen0816@gmail.com>
2019-05-22 14:29:54 -0700

    update build

--- jenkins.sh
+++ jenkins.sh
@@ -20,6 +20,8 @@ echo "repos=c('http://cran.fhcrc.org', '${RAN}'))" >> installPackages.R
 R --vanilla < installPackages.R
 rm installPackages.R
 
+export label
+export RVERS	export RVERS=$(echo $label | awk -F[-] '{print $3}')
 PACKAGE_NAME=synapser
 
 # if version is specified, build the given version
