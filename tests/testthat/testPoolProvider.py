61f7fac29ad771a4f3f8c58fc9e658271cbeb19d
Jordan Kiang <jordan.kiang@sagebase.org>
2020-04-30 07:06:20 -0700

    pool_provider moved

--- tests/testthat/testPoolProvider.py
+++ tests/testthat/testPoolProvider.py
@@ -4,7 +4,7 @@ def print_x(x):
   print(x)
 
 def test():
-  mp = synapseclient.pool_provider.get_pool()
+  mp = synapseclient.core.pool_provider.get_pool()
   try:
     mp.map(print_x, range(1, 100))
   finally:
