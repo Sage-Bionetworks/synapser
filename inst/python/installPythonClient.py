52307b319711fb4b3cf6493c0b303e3933a9b7e4
Jordan Kiang <jordan.kiang@sagebase.org>
2020-04-30 19:31:17 -0700

    use a subprocess to invoke pip for some packages, side load others. other combinations/options seem to break in various ways

--- inst/python/installPythonClient.py
+++ inst/python/installPythonClient.py
@@ -15,15 +15,9 @@ import importlib
 import pkg_resources
 import glob
 import zipfile
+import inspect
+import subprocess
 from patchStdoutStdErr import patch_stdout_stderr
-
-# in a stable way across python versions. the typical approach is to
-# call pip in a subprocess using sys.executable, but running inside
-# PythonEmbedInR sys.executable may not be what we want.
-try:
-    from pip import main as pipmain
-except ImportError:
-    from pip._internal import main as pipmain
 
 def localSitePackageFolder(root):
     if os.name=='nt':
@@ -49,73 +43,98 @@ def addLocalSitePackageToPythonPath(root):
         os.environ['PYTHONPATH'] += os.pathsep+eggpath
         sys.path.append(eggpath)
 
+
+def _find_python_interpreter():
+    # helper heuristic to find the bundled python interpreter binary associated
+    # with PythonEmbedInR. we need it in order to be able to invoke pip via
+    # a subprocess. it's not in the same place because of how we build
+    # PythonEmbedInR differently the OSes.
+
+    possible_interpreter_filenames = [
+        'python',
+        'python{}'.format(sys.version_info.major),
+        'python{}.{}'.format(sys.version_info.major, sys.version_info.minor),
+    ]
+    possible_interpreter_filenames.extend(['{}.exe'.format(f) for f in possible_interpreter_filenames])
+    possible_interpreter_filenames.extend([os.path.join('bin', f).format(f) for f in possible_interpreter_filenames])
+
+    last_path = None
+    path = inspect.getfile(os)
+    while(path and path != last_path):
+        for f in possible_interpreter_filenames:
+            file_path = os.path.join(path, f)
+            if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                return file_path
+
+        last_path = path
+        path = os.path.dirname(path)
+
+    # if we didn't find anything we'll hope there is any 'python3' interpreter on the path.
+    # we're just going to use it to install some modules into a specific directory
+    # so it doesn't actually even have to be the one bundled with PythonEmbedInR
+    return 'python{}'.format(sys.version_info.major)
+
+
 def main(path):
     patch_stdout_stderr()
-    
+
     path = pkg_resources.normalize_path(path)
     moduleInstallationPrefix=path+os.sep+"inst"
 
     localSitePackages=localSitePackageFolder(moduleInstallationPrefix)
-    
+
     addLocalSitePackageToPythonPath(moduleInstallationPrefix)
 
     os.makedirs(localSitePackages)
 
-    # The preferred approach to install a package is to use pip...
-    call_pip('pandas==0.22', localSitePackages)
-#     # check that the installation worked
-#    addLocalSitePackageToPythonPath(moduleInstallationPrefix)
-#     import pandas# This fails intermittently
+    # the least error prone way to install packages at runtime is by invoking
+    # pip via a subprocess, although it's not straightforward to do so here.
+    pip_packages = [
+        'pandas==0.22',
+        'synapseclient==2.0.0',
+    ]
 
-    # ...but - for some reason - pip breaks when we install the python synapse client
-    # So we use 'setup' directly
-    packageName = "synapseclient-2.0.0"
-    
-    if 'PYTHON_CLIENT_GITHUB_USERNAME' in os.environ and 'PYTHON_CLIENT_GITHUB_BRANCH' in os.environ:
-        pythonClientGithubUsername = os.environ['PYTHON_CLIENT_GITHUB_USERNAME']
-        pythonClientGithubBranch = os.environ['PYTHON_CLIENT_GITHUB_BRANCH']
-        archivePrefix="synapsePythonClient-"+pythonClientGithubBranch
-        archiveSuffix=".zip"
-        url="https://github.com/"+pythonClientGithubUsername+"/synapsePythonClient/archive/"+pythonClientGithubBranch+archiveSuffix
+    if platform.system() != 'Windows':
+        # on linux and mac we can install these via a pip subprocess...
+        pip_packages.extend([
+           "MarkupSafe==1.0",
+           "Jinja2==2.8.1",
+        ])
+
     else:
-        archivePrefix=packageName
-        urlPrefix = "https://files.pythonhosted.org/packages/6f/ea/eb321bc1449d4ec17b46c55f57ac8184da3d082cbd8833eff6ba8aafc175/"
-        archiveSuffix = ".tar.gz"
-        url = urlPrefix+archivePrefix+archiveSuffix
+        # ...but on windows installing via a subprocess breaks seemingly with the wheel install,
+        # so we use the creative sideloading as below.
+
+        # Jinja2 depends on MarkupSafe
+        packageName = "MarkupSafe-1.0"
+        linkPrefix = "https://pypi.python.org/packages/4d/de/32d741db316d8fdb7680822dd37001ef7a448255de9699ab4bfcbdf4172b/"
+        installedPackageFolderName="markupsafe"
+        simplePackageInstall(packageName, installedPackageFolderName, linkPrefix, path, localSitePackages)
+        addLocalSitePackageToPythonPath(moduleInstallationPrefix)
+        #import markupsafe  # This fails intermittently
     
-    installPackage(packageName, url, archivePrefix, archiveSuffix, path, moduleInstallationPrefix)
-        
-    # check that the installation worked
-    addLocalSitePackageToPythonPath(moduleInstallationPrefix)
-    import synapseclient
-    
-    # When trying to 'synStore' a table we get the error:
-    # pandas.Styler requires jinja2. Please install with `conda install Jinja2`
-    # So let's install Jinja2 here:
-    # https://stackoverflow.com/questions/43163201/pyinstaller-syntax-error-yield-inside-async-function-python-3-5-1
+        packageName = "Jinja2-2.8.1"
+        linkPrefix = "https://pypi.python.org/packages/5f/bd/5815d4d925a2b8cbbb4b4960f018441b0c65f24ba29f3bdcfb3c8218a307/"
+        installedPackageFolderName="jinja2"
+        simplePackageInstall(packageName, installedPackageFolderName, linkPrefix, path, localSitePackages)
+        addLocalSitePackageToPythonPath(moduleInstallationPrefix)
+        #import jinja2 # This fails intermittently
 
-    # Jinja2 depends on MarkupSafe
-    packageName = "MarkupSafe-1.0"
-    linkPrefix = "https://pypi.python.org/packages/4d/de/32d741db316d8fdb7680822dd37001ef7a448255de9699ab4bfcbdf4172b/"
-    installedPackageFolderName="markupsafe"
-    simplePackageInstall(packageName, installedPackageFolderName, linkPrefix, path, localSitePackages)
-    addLocalSitePackageToPythonPath(moduleInstallationPrefix)
-    #import markupsafe  # This fails intermittently
+    # the recommended way to call pip at runtime is by invoking a subprocess,
+    # but that's complicated by the fact that we don't know where the python
+    # interpreter is. usually you can do sys.executable but in the embedded
+    # context sys.executable is R, not python. So we do a heuristic that to
+    # find the interpreter. this seems to work better here than calling main
+    # on pip directly which doesn't work for some of these packages (separately
+    # from the other issues above...)
+    interpreter = _find_python_interpreter()
+    for package in pip_packages:
+        rc = subprocess.call([interpreter, "-m", "pip", "install", package, "--upgrade", "--quiet", "--target", localSitePackages])
+        if rc != 0:
+            raise Exception("pip.main returned {} when installing {}".format(rc, package))
 
-    packageName = "Jinja2-2.8.1"
-    linkPrefix = "https://pypi.python.org/packages/5f/bd/5815d4d925a2b8cbbb4b4960f018441b0c65f24ba29f3bdcfb3c8218a307/"
-    installedPackageFolderName="jinja2"
-    simplePackageInstall(packageName, installedPackageFolderName, linkPrefix, path, localSitePackages)
-    addLocalSitePackageToPythonPath(moduleInstallationPrefix)
-    #import jinja2 # This fails intermittently
+    addLocalSitePackageToPythonPath(moduleInstallationPrefix) 
 
-# pip installs in the wrong place (ends up being in the PythonEmbedInR package rather than this one)
-def call_pip(packageName, target):
-        rc = pipmain(['install', packageName, '--upgrade', '--quiet', '--target', target])
-        if rc!=0:
-            raise Exception('pip.main returned '+str(rc))
-
-    
 # unzip directly into localSitePackages/installedPackageFolderName
 # This is a workaround for the cases in which 'pip' and 'setup.py' fail.
 # (They fail for MarkupSafe and Jinja2, without providing any info about what went wrong.)
@@ -143,43 +162,3 @@ def simplePackageInstall(packageName, installedPackageFolderName, linkPrefix, pa
     shutil.rmtree(packageDir)
     
     sys.path.append(localSitePackages+os.sep+installedPackageFolderName)
-
-def installPackage(packageName, url, archivePrefix, archiveSuffix, path, moduleInstallationPrefix):
-    # download 
-    zipFileName = archivePrefix + archiveSuffix
-    localZipFile = path+os.sep+zipFileName
-    x = urllib.request.urlopen(url)
-    saveFile = open(localZipFile,'wb')
-    saveFile.write(x.read())
-    saveFile.close()
-    
-    if archiveSuffix==".tar.gz":
-        tar = tarfile.open(localZipFile)
-        tar.extractall(path=path)
-        tar.close()
-    elif archiveSuffix==".zip":
-        zipFile=zipfile.ZipFile(localZipFile)
-        zipFile.extractall(path=path)
-        zipFile.close()
-    else:
-        raise Exception("Unexpected suffix "+suffix)
-    
-    os.remove(localZipFile)
-        
-    packageDir = path+os.sep+archivePrefix
-    os.chdir(packageDir)
-    
-    orig_sys_path = sys.path
-    orig_sys_argv = sys.argv
-    sys.path = ['.'] + sys.path
-    sys.argv = ['setup.py', 'install', '--prefix='+moduleInstallationPrefix]
-
-    try:
-        importlib.import_module("setup")
-    finally:
-        sys.path=orig_sys_path
-        sys.argv=orig_sys_argv
-        # leave the folder we're about to delete
-        os.chdir(path)
-        shutil.rmtree(packageDir)
-
