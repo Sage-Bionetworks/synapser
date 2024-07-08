7dae286ebd6c03192c0cd9b45f44e131cdf1cb7c
danlu1 <dan.lu@sagebase.org>
2024-07-08 18:13:28 +0000

    update reference docs for new functions

--- R/zzz.R
+++ R/zzz.R
@@ -12,28 +12,28 @@
       # Ideally we would source tools/installPythonClient.R to not
       # have to duplicate the synapseclient install code
       # system2(paste("Rscript ", getwd(), "/tools/installPythonClient.R ", getwd(), sep=""))
-      PYTHON_CLIENT_VERSION <- '4.0.0'
+      PYTHON_CLIENT_VERSION <- '4.3.0'
       # reticulate::virtualenv_create('r-reticulate')
       # reticulate::use_virtualenv('r-reticulate')
-      reticulate::py_install(c("requests<3", "pandas~=2.0.0", "pysftp", "jinja2", "markupsafe"))
+      reticulate::py_install(c("requests<3", "pandas==2.0.3", "pysftp", "jinja2", "markupsafe"))
       reticulate::py_install(c(paste("synapseclient==", PYTHON_CLIENT_VERSION, sep="")), pip=T)
       reticulate::py_run_string("import synapseclient")
     }
   )
-
+  
   reticulate::py_run_string(sprintf("synapserVersion = 'synapser/%s' ", utils::packageVersion("synapser")))
   reticulate::py_run_string("synapseclient.USER_AGENT['User-Agent'] = synapserVersion + ' '+ synapseclient.USER_AGENT['User-Agent']")
   reticulate::py_run_string("synapseclient.core.config.single_threaded = True")
-  reticulate::py_run_string("syn=synapseclient.Synapse(skip_checks=True)")
+  reticulate::py_run_string("syn=synapseclient.Synapse(skip_checks=True,debug=True)")
   # make syn available in the global environment
   syn <<- reticulate::py_eval("syn")
-
+  
   .addPythonAndFoldersToSysPath(system.file(package = "synapser"))
   .defineRPackageFunctions()
   # .defineOverloadFunctions() must come AFTER .defineRPackageFunctions()
   # because it redefines selected generic functions
   .defineOverloadFunctions()
-
+  
   # mute Python warnings
   reticulate::py_run_string("import warnings")
   reticulate::py_run_string("warnings.filterwarnings('ignore')")
@@ -98,7 +98,7 @@
   2) Not discriminate, identify, or recontact individuals or groups represented by the data.
   3) Use and contribute only data de-identified to HIPAA standards.
   4) Redistribute data only under these same terms of use.\n"
-
+  
   .checkForUpdate()
   packageStartupMessage(tou)
 }
@@ -128,7 +128,7 @@
       Table(schema, file)
     }
   )
-
+  
   methods::setMethod(
     f = "synBuildTable",
     signature = c("ANY", "ANY", "data.frame"),
@@ -138,7 +138,7 @@
       synBuildTable(name, parent, file)
     }
   )
-
+  
   methods::setClass("CsvFileTable")
   methods::setMethod(
     f = "as.data.frame",
@@ -147,7 +147,7 @@
       x$asDataFrame()
     }
   )
-
+  
   methods::setClass("GeneratorWrapper")
   methods::setMethod(
     f = "as.list",
@@ -156,14 +156,14 @@
       x$asList()
     }
   )
-
+  
   methods::setGeneric(
     name = "nextElem",
     def = function(x) {
       standardGeneric("nextElem")
     }
   )
-
+  
   methods::setMethod(
     f = "nextElem",
     signature = c(x = "GeneratorWrapper"),
--- tools/installPythonClient.R
+++ tools/installPythonClient.R
@@ -5,13 +5,13 @@
 # Author: bhoff
 ###############################################################################
 
-PYTHON_CLIENT_VERSION <- '4.0.0'
+PYTHON_CLIENT_VERSION <- '4.3.0'
 
 args <- commandArgs(trailingOnly = TRUE)
 baseDir<-args[1]
 
 if (is.null(baseDir) || is.na(baseDir) || !file.exists(baseDir)) {
-	stop(paste("baseDir", baseDir, "is invalid"))
+  stop(paste("baseDir", baseDir, "is invalid"))
 }
 print("*** Using Python Configuration:")
 # reticulate::virtualenv_create('r-reticulate')
@@ -19,5 +19,5 @@ print("*** Using Python Configuration:")
 reticulate::py_config()
 reticulate::py_run_string("import sys")
 reticulate::py_run_string(sprintf("sys.path.append(\"%s\")", file.path(baseDir, "inst", "python")))
-reticulate::py_install(c("requests<3", "pandas~=2.0.0", "pysftp", "jinja2", "markupsafe"))
+reticulate::py_install(c("requests<3", "pandas==2.0.3", "pysftp", "jinja2", "markupsafe"))
 reticulate::py_install(c(paste("synapseclient==", PYTHON_CLIENT_VERSION, sep="")), pip=T)
