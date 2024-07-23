2ab2ef2adda507743e1e69c5afadb02ac3ffb5f6
danlu1 <dan.lu@sagebase.org>
2024-07-23 16:48:16 +0000

    use virtural env

--- tools/installPythonClient.R
+++ tools/installPythonClient.R
@@ -15,9 +15,9 @@ if (is.null(baseDir) || is.na(baseDir) || !file.exists(baseDir)) {
 }
 print("*** Using Python Configuration:")
 # reticulate::virtualenv_create('r-reticulate')
-# reticulate::use_virtualenv('r-reticulate')
+reticulate::use_virtualenv('r-reticulate')
 reticulate::py_config()
 reticulate::py_run_string("import sys")
-#reticulate::py_run_string(sprintf("sys.path.append(\"%s\")", file.path(baseDir, "inst", "python")))
+reticulate::py_run_string(sprintf("sys.path.append(\"%s\")", file.path(baseDir, "inst", "python")))
 reticulate::py_install(c("requests<3", "pandas==2.0.3", "pysftp", "jinja2", "markupsafe","numpy==1.24.4"))
 reticulate::py_install(c(paste("synapseclient==", PYTHON_CLIENT_VERSION, sep="")), pip=T)
