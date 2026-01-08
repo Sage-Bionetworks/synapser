a2b5d34c63233845f3c877ad330a250e65360c04
danlu1 <dan.lu@sagebase.org>
2026-01-07 17:03:03 -0800

    upgrade synapseclient

--- tools/installPythonClient.R
+++ tools/installPythonClient.R
@@ -5,7 +5,7 @@
 # Author: bhoff
 ###############################################################################
 
-PYTHON_CLIENT_VERSION <- '4.4.0'
+PYTHON_CLIENT_VERSION <- '4.10.0'
 
 args <- commandArgs(trailingOnly = TRUE)
 baseDir<-args[1]
@@ -14,10 +14,8 @@ if (is.null(baseDir) || is.na(baseDir) || !file.exists(baseDir)) {
   stop(paste("baseDir", baseDir, "is invalid"))
 }
 print("*** Using Python Configuration:")
-# reticulate::virtualenv_create('r-reticulate')
-#reticulate::use_virtualenv('r-reticulate')
 reticulate::py_config()
 reticulate::py_run_string("import sys")
 reticulate::py_run_string(sprintf("sys.path.append(\"%s\")", file.path(baseDir, "inst", "python")))
-reticulate::py_install(c("pandas>=1.5,<=2.0.3", "jinja2", "markupsafe","numpy<=1.24.4"))
+reticulate::py_install(c("pandas>=1.5,<3.0", "jinja2", "markupsafe","numpy<2.0"))
 reticulate::py_install(c(paste("synapseclient==", PYTHON_CLIENT_VERSION, sep="")), pip=T)
