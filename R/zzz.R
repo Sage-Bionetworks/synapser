46c10000e04ed23cd8a65aca649e2dcacd5feda3
danlu1 <dan.lu@sagebase.org>
2026-05-06 11:30:04 -0700

    upgrade to synapseclient 4.12

--- R/zzz.R
+++ R/zzz.R
@@ -12,8 +12,8 @@
       # Ideally we would source tools/installPythonClient.R to not
       # have to duplicate the synapseclient install code
       # system2(paste("Rscript ", getwd(), "/tools/installPythonClient.R ", getwd(), sep=""))
-      PYTHON_CLIENT_VERSION <- '4.4.2'
-      reticulate::py_install(c("pandas>=1.5,<=2.0.3", "jinja2", "markupsafe","numpy<=1.24.4"))
+      PYTHON_CLIENT_VERSION <- 'v4.12'
+      reticulate::py_install(c("pandas>=1.5,<=3.0", "jinja2", "markupsafe"))
       reticulate::py_install(c(paste("synapseclient==", PYTHON_CLIENT_VERSION, sep="")), pip=T)
       reticulate::py_run_string("import synapseclient")
     }
