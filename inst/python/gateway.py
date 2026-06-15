f68c7f6c029f31c4f5b0986c28fb0bd4e031b823
danlu1 <dan.lu@sagebase.org>
2026-06-15 09:52:02 -0700

    add convenient function to convert R double to python integer

--- inst/python/gateway.py
+++ inst/python/gateway.py
@@ -32,11 +32,29 @@ def generatorModifier(g):
         return g
 
 
+def _coerce_numeric(obj):
+    """Recursively convert whole-number R doubles (e.g. 1.0) to Python ints.
+
+    reticulate passes R numeric vectors as Python floats. Many Synapse API
+    parameters (version_number, principal_id, …) expect int/Long. This avoids
+    requiring callers to write 1L in R everywhere since R passes bare 1 to Python
+    as a double (1.0), but the Synapse API expects a Long. 
+    """
+    if isinstance(obj, float) and obj.is_integer():
+        return int(obj)
+    if isinstance(obj, (list, tuple)):
+        coerced = (_coerce_numeric(x) for x in obj)
+        return type(obj)(coerced)
+    if isinstance(obj, dict):
+        return {k: _coerce_numeric(v) for k, v in obj.items()}
+    return obj
+
+
 # expects a dict with the keys: method (a list of [object, method name]), args, and kwargs
 def invoke(**kwargs):
     patch_stdout_stderr()
     method = kwargs["method"]
-    args = kwargs["args"]
-    kw = dict(kwargs["kwargs"])
+    args = _coerce_numeric(kwargs["args"])
+    kw = _coerce_numeric(dict(kwargs["kwargs"]))
     method_to_call = getattr(method[0], method[1])
     return generatorModifier(abbreviateStackTrace(lambda: method_to_call(*args, **kw)))
