0d4d83e7cecd0baf600ced0d22c49fe9f814c0ad
danlu1 <dan.lu@sagebase.org>
2026-06-18 17:17:55 -0700

    update generateRdFiles as settings in generateRWrappers

--- tools/createRdFiles.R
+++ tools/createRdFiles.R
@@ -10,28 +10,35 @@ source(sprintf("%s/R/shared.R", srcRootDir))
 source(sprintf("%s/R/PythonPkgWrapperUtils.R", srcRootDir))
 
 reticulate::py_run_string("import sys")
-reticulate::py_run_string(sprintf("sys.path.append(\"%s\")", file.path(srcRootDir, "inst", "python")))
-## all functions in synapseclient.Synapse module
-generateRdFiles(srcRootDir,
-                pyPkg = "synapseclient",
-                container = "synapseclient.Synapse",
-                functionFilter = .synapseClassFunctionFilter,
-                functionPrefix = "syn")
+reticulate::py_run_string(sprintf(
+    "sys.path.append(\"%s\")",
+    file.path(srcRootDir, "inst", "python")
+))
+
+generateRdFiles(
+    srcRootDir,
+    pyPkg = "synapseclient",
+    container = "synapseclient.Synapse",
+    functionFilter = .synapseClassFunctionFilter,
+    functionPrefix = "syn"
+)
 reticulate::py_run_string("import synapseclient.operations")
-generateRdFiles(srcRootDir,
-                pyPkg = "synapseclient",
-                container = "synapseclient.operations",
-                functionFilter = .removeAsyncFunctionFilter,
-                keepContent = TRUE,
-                functionPrefix = "syn",
-                generateFunctionalInterface = TRUE,
-                )
-generateRdFiles(srcRootDir,
-                pyPkg = "synapseclient",
-                container = "synapseclient.models",
-                functionFilter = .removeAsyncFunctionFilter,
-                classFilter = .synapseModelClassFilter,
-                keepContent = TRUE,
-                functionPrefix = "syn",
-                generateFunctionalInterface = TRUE,
-                )
+generateRdFiles(
+    srcRootDir,
+    pyPkg = "synapseclient",
+    container = "synapseclient.operations",
+    functionFilter = .operationsFunctionNamesFilter,
+    keepContent = TRUE,
+    functionPrefix = "syn"
+)
+generateRdFiles(
+    srcRootDir,
+    pyPkg = "synapseclient",
+    container = "synapseclient.models",
+    functionFilter = .removeAsyncFunctionFilter,
+    classFilter = .synapseModelClassFilter,
+    keepContent = TRUE,
+    functionPrefix = "syn",
+    generateFunctionalInterface = TRUE,
+    functionNameMapping = .synapseClientModelsMapping()
+)
